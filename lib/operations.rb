88f1b98281d38920bf665b0622e91429764df2a8
Merline Belizaire <merline@noukod.com>
2018-09-19 16:17:29 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,11 @@
 def unsafe?(speed)
 speed = 60
-speed < 40 <=> 60? true : false
+speed < 40, 60? true : false
 end
 
 
 
 def not_safe?(speed)
 speed = 50
-speed < 35 <=> 50 ? true : false
+speed < 35, 50 ? true : false
 end
